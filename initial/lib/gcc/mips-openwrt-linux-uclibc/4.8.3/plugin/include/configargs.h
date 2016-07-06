12303b1fc401dab7b04b3faf0b0e67de08142414
duongnh <duongnh@bkav.com>
2016-07-06 11:07:13 +0700

    Add toolchain
    Signed-off-by: duongnh <duongnh@bkav.com>

---
+++ initial/lib/gcc/mips-openwrt-linux-uclibc/4.8.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/duongnh/openwrt/trunk/build_dir/toolchain-mips_34kc_gcc-4.8-linaro_uClibc-0.9.33.2/gcc-linaro-4.8-2014.04/configure --with-bugurl=https://dev.openwrt.org/ --with-pkgversion='OpenWrt/Linaro GCC 4.8-2014.04 r49378' --prefix=/home/duongnh/openwrt/trunk/staging_dir/toolchain-mips_34kc_gcc-4.8-linaro_uClibc-0.9.33.2 --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=mips-openwrt-linux-uclibc --with-gnu-ld --enable-target-optspace --disable-libgomp --disable-libmudflap --disable-multilib --disable-nls --with-host-libstdcxx=-lstdc++ --with-float=soft --with-gmp=/home/duongnh/openwrt/trunk/staging_dir/host --with-mpfr=/home/duongnh/openwrt/trunk/staging_dir/host --with-mpc=/home/duongnh/openwrt/trunk/staging_dir/host --disable-decimal-float --with-mips-plt --disable-libssp --disable-__cxa_atexit --with-newlib --with-sysroot=/home/duongnh/openwrt/trunk/staging_dir/toolchain-mips_34kc_gcc-4.8-linaro_uClibc-0.9.33.2 --enable-languages=c --disable-shared --disable-threads";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "float", "soft" }, { "llsc", "llsc" }, { "mips-plt", "plt" } };
